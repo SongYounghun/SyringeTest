e431dce1e28e3a313fdf7dbec3ec5b6d3a91f302
yhsong <yhsong@ati2000.co.kr>
2018-09-07 17:43:50 +0900

    add recipe function

--- SyringeTest/MainForm.cs
+++ SyringeTest/MainForm.cs
@@ -9,10 +9,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace SyringeTest
 {
@@ -200,6 +202,167 @@ namespace SyringeTest
             {
                 e.Cancel = true;
                 return;
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LoadSeqRecipe();
+        }
+
+        private void LoadSeqRecipe()
+        {
+            string recPath = Application.StartupPath + "\\SeqRecipe";
+            if (Directory.Exists(recPath) && Directory.GetFiles(recPath).Length > 0)
+            {
+                String[] strfilePaths = Directory.GetFileSystemEntries(recPath);
+                for (int i = 0; i < strfilePaths.Length; i++)
+                {
+                    string path = Path.GetFileNameWithoutExtension(strfilePaths[i]);
+                    listView_seq_recipe.Items.Add(path);
+                    //if (MessageBox.Show("Do you want to load sequence?", "", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+                    //{
+                    //    m_seqMng.ClearSeq();
+                    //    LoadSeqXml(path);
+                    //}
+                }
+            }
+        }
+
+        private void LoadSeqXml(string path, bool isUpdateUI)
+        {
+            try
+            {
+                XDocument doc = XDocument.Load(path);
+                XElement root = doc.Root;
+
+                if (root != null && !root.IsEmpty)
+                {
+                    IEnumerator<XElement> xSequences = root.Elements().GetEnumerator();
+                    while (xSequences.MoveNext())
+                    {
+                        XElement xSeq = xSequences.Current;
+
+                        switch (xSeq.Name.LocalName)
+                        {
+                            case "Delay":
+                                {
+                                    if (xSeq.Element("delay_time") != null)
+                                    {
+                                        int nDelayTime = Convert.ToInt32(xSeq.Element("delay_time").Value);
+
+                                    }
+                                }
+                                break;
+
+                            case "IO":
+                                {
+                                    if (xSeq.Element("delay_time") != null)
+                                    {
+
+                                    }
+                                }
+                                break;
+
+                            case "Motion":
+                                {
+
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.ToString());
+            }
+        }
+
+        private void SaveSeqXml(string path)
+        {
+            try
+            {
+                XDocument doc = new XDocument();
+                XElement root = new XElement("Sequence");
+                for (int i = 0; i < m_seqMng.ListSequence.Count; i++)
+                {
+                    XElement xSeq = null;
+                    SequenceManager.SEQ_LIST listSeq = m_seqMng.ListSequence[i];
+                    switch (listSeq.seq)
+                    {
+                        case SequenceManager.SEQUENCE.Delay:
+                            {
+                                int delTime = (int)listSeq.data;
+                                xSeq = new XElement("Delay");
+                                xSeq.Add(new XElement("delay_time", delTime));
+                            }
+                            break;
+
+                        case SequenceManager.SEQUENCE.IO:
+                            {
+                                SyringeTest.SequenceManager.SEQ_IO dataIO = (SyringeTest.SequenceManager.SEQ_IO)listSeq.data;
+                                xSeq = new XElement("IO");
+                                xSeq.Add(new XElement("nIoNum", dataIO.nIoNum));
+                                xSeq.Add(new XElement("bOn", dataIO.bOn));
+                            }
+                            break;
+
+                        case SequenceManager.SEQUENCE.Motion:
+                            {
+                                SyringeTest.SequenceManager.SEQ_MOTION dataMot = (SyringeTest.SequenceManager.SEQ_MOTION)listSeq.data;
+                                xSeq = new XElement("Motion");
+                                xSeq.Add(new XElement("nAxisNum", dataMot.nAxisNum));
+                                xSeq.Add(new XElement("dPos", dataMot.dPos));
+                            }
+                            break;
+                    }
+
+                    if (xSeq != null)
+                    {
+                        root.Add(xSeq);
+                    }
+                }
+
+                doc.Add(root);
+                doc.Save(path);
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.ToString());
+            }
+        }
+
+        private void button_seq_save_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBox_seq_name.Text))
+            {
+                if (!Directory.Exists(Application.StartupPath + "\\SeqRecipe"))
+                {
+                    Directory.CreateDirectory(Application.StartupPath + "\\SeqRecipe");
+                }
+                SaveSeqXml(Application.StartupPath + "\\SeqRecipe\\" + textBox_seq_name.Text);
+                listView_seq_recipe.Items.Add(textBox_seq_name.Text);
+            }
+        }
+
+        private void listView_seq_recipe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView_seq_recipe.Items.Count > 0)
+            {
+                if (listView_seq_recipe.SelectedItems.Count > 0)
+                {
+                    string path = listView_seq_recipe.SelectedItems[0].Text;
+                    if (Directory.Exists(Application.StartupPath + "\\SeqRecipe"))
+                    {
+                        if (MessageBox.Show("Do you want to load sequence?", "", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+                        {
+                            LoadSeqXml(Application.StartupPath + "\\SeqRecipe\\" + path, true);
+                            textBox_seq_name.Text = path;
+                        }
+                        
+                    }
+                }
             }
         }
     }
